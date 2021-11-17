08a3b4607299c00d0b858a3d4b7cb3c996cab4f6
Illensky <alexis.laroche.02240@gmail.com>
2021-11-17 19:00:07 +0100

    Exercice done

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,10 @@
-let monDiv = .createElement('div');
-
+let monDiv = document.createElement('div');
+monDiv.innerText = "Ce div a été ajouté en utilisant javascript"
+monDiv.style.backgroundColor = "red"
+monDiv.style.width = "100px"
+monDiv.style.height = "100px"
 document.getElementById('viewport').appendChild(monDiv);
+document.getElementById('aSupprimer').remove();
 
 
 
